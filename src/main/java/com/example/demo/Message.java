7a890c736fdcf36571bd9a1e5f6a9c29ef95195e
GBTC408014UR <GBTC408014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-15 15:29:28 -0400

    first section of bullhorn to submit

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.text.DateFormat;
+
+@Entity()
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+                                //@CreatedDate
+    private String posteddate;  //date format if time
+
+    @NotNull
+    private String sentby;
+
+
+    public Message() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(String posteddate) {
+        this.posteddate = posteddate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
